1249f449e5235d681ab115aa0d809fd5aaade1f9
stefan hengl <stefan.hengl@gmail.com>
2019-08-05 20:23:01 +0200

    chore: add LICENSE, .travis.yml, update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject fields "0.1.0-SNAPSHOT"
-  :description "A library to select keys from nested maps. Inpsired by REST field query-parameter"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A library to select keys from nested maps. Inspired by the fields request parameter propsed by Google."
+  :scm {:name "git" :url "https://github.com/stefanhengl/fields" }
+  :url "https://github.com/stefanhengl/fields"
+  :license {:name "MIT"
+            :url "https://github.com/stefanhengl/fields/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]]
   :repl-options {:init-ns fields.core})

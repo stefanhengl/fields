187ddc58997a47092b6dcfb92f01b372eebd8afb
stefan hengl <stefan.hengl@gmail.com>
2019-08-09 22:13:52 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fields "0.1.0"
+(defproject fields "0.1.1"
   :description "Like select-keys but for nested maps. Inspired by the fields request parameter proposed by Google."
   :scm {:name "git" :url "https://github.com/stefanhengl/fields" }
   :url "https://github.com/stefanhengl/fields"
